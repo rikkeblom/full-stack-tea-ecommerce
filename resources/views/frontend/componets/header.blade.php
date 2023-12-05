f4e870d626d71c90ee1f304492bc9a865a024559
Victoria <vigi2586@stud.kea.dk>
2023-12-05 15:43:34 +0100

    global header is done

---
+++ resources/views/frontend/componets/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+    <!-- Links to pages -->
+        <div class="links">
+            <a href="">Shop Te</a>
+            <a href="">Om Rivitea</a>
+        </div>
+    <!--/ Links to pages -->
+    <!-- Logo -->
+        <div>
+            <a href="home">
+                @include('frontend.componets.svger.logo')
+            </a>
+        </div>
+    <!--/ Logo -->
+    <!-- Cart and Search icons -->
+        <div class="svger">
+            <!-- Search icon -->
+            <div>
+                @include('frontend.componets.svger.search')
+            </div>
+            <!--/ Search icon -->
+            <!-- Cart icon -->
+            <div>
+                @include('frontend.componets.svger.cart')
+            </div>
+            <!--/ Cart icon -->
+        </div>
+    <!--/ Cart and Search icons -->
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('frontend.componets.head')
+</head>
+    @include('frontend.componets.header')
+<body>
+    <h1>Home</h1>
+</body>
+</html>
